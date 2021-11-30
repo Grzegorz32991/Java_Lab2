5f720d4c5a89bfbcb827f759e4cd825ba8176263
Grzegorz Sirocki <daddyip@wp.pl>
2021-11-30 16:51:03 +0100

    ćw 4

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,22 @@ public class Main {
         b) w drugiej pętli continue
         c) w trzeciej pętli return
         */
+      for (int i = 0; i <= 100; i++) {
+           if (i % 8 == 0)
+               continue;
+           System.out.print(i + " ");
+       }
 
+       for (int i = 0; i <= 125; i++) {
+           if (i == 85)
+              break;
+           System.out.print(i + " ");
+       }
+        for (int i = 0; i <= 100; i++) {
+            if (i == 33)
+                return;
+            System.out.print(i + " ");
+        }
 
     }
 }
